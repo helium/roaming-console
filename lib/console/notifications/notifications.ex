404d50e0a644fe33e1f4a49254d93cfd3554f58e
victor <victor@helium.com>
2021-09-30 09:25:31 -0700

    Set up notif backend

---
+++ lib/console/notifications/notifications.ex
@@ -0,0 +1,21 @@
+defmodule Console.Notifications do
+  import Ecto.Query, warn: false
+  alias Console.Repo
+
+  alias Console.Notifications.Notification
+  alias Console.Organizations.Organization
+
+  def create_notification(attrs \\ %{}, %Organization{} = organization) do
+    attrs = Map.merge(attrs, %{"organization_id" => organization.id})
+
+    %Notification{}
+    |> Notification.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_notification(attrs \\ %{}, %Notification{} = notification) do
+    notification
+    |> Notification.changeset(attrs)
+    |> Repo.update()
+  end
+end
