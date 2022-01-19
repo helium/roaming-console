1809ddf4c65156e5453743636084120b8bde8f0f
Alejandra Buznego <abuznego23@gmail.com>
2022-01-19 15:55:07 -0700

    add migration

--- priv/repo/migrations/20220119224833_change_net_id_to_integer.exs
+++ priv/repo/migrations/20220119224833_change_net_id_to_integer.exs
@@ -2,6 +2,12 @@ defmodule Console.Repo.Migrations.ChangeNetIdToInteger do
   use Ecto.Migration
 
   def change do
+    alter table("net_ids") do
+      modify :value, :integer, null: false
+    end
 
+    alter table("packets") do
+      modify :net_id, :integer, null: false
+    end
   end
 end
