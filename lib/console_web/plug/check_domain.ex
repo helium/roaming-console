16c25cee8a4e517a99f0d43ffe729888e575de7c
victor <victor@helium.com>
2022-02-21 14:38:21 -0800

    Add allowed eu domains

--- lib/console_web/plug/check_domain.ex
+++ lib/console_web/plug/check_domain.ex
@@ -16,6 +16,8 @@ defmodule ConsoleWeb.Plug.CheckDomain do
       case conn.host do
         "localhost" -> conn
         "roaming.helium.com" -> conn
+        "roaming-eu.helium.com" -> conn
+        "roaming-console-eu.herokuapp.com" -> conn
         _ ->
           if Application.get_env(:console, :self_hosted) == nil do
             conn
