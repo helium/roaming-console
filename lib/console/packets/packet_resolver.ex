0b356e9ab00bee16fec1caa8404da74496c5e3cb
Alejandra Buznego <abuznego23@gmail.com>
2022-02-23 11:10:16 -0700

    fix

--- lib/console/packets/packet_resolver.ex
+++ lib/console/packets/packet_resolver.ex
@@ -1,25 +1,11 @@
 defmodule Console.Packets.PacketResolver do
-  alias Console.Repo
-  import Ecto.Query
+  alias Console.Packets
 
   def get_packets(_, %{context: %{current_organization: current_organization}}) do
     current_unix = DateTime.utc_now() |> DateTime.to_unix(:millisecond)
     unix1d = current_unix - 86400000
-    {:ok, organization_id} = Ecto.UUID.dump(current_organization.id)
+    packets = Packets.get_packets_per_netid(current_organization.id, unix1d)
 
-    sql = """
-      SELECT net_id, reported_at_epoch
-      FROM packets
-      WHERE organization_id = $1 AND reported_at_epoch > $2
-      ORDER BY reported_at_epoch DESC
-    """
-    result = Ecto.Adapters.SQL.query!(Console.Repo, sql, [organization_id, unix1d])
-    rows =
-      result.rows
-      |> Enum.map(fn r ->
-        %{ net_id: Enum.at(r, 0), reported_at_epoch: Enum.at(r, 1) }
-      end)
-
-    {:ok, rows}
+    {:ok, packets}
   end
 end
--- lib/console/packets/packets.ex
+++ lib/console/packets/packets.ex
@@ -10,4 +10,11 @@ defmodule Console.Packets do
     |> Packet.changeset(attrs)
     |> Repo.insert()
   end
+
+  def get_packets_per_netid(organization_id, epoch_since) do
+    query = from p in Packet,
+      select: %{reported_at_epoch: p.reported_at_epoch, net_id: p.net_id},
+      where: p.organization_id == ^organization_id and p.reported_at_epoch > ^epoch_since
+    Repo.all(query)
+  end
 end
