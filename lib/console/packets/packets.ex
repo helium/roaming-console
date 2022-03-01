d41931ae8fc9f1f2409877b0aa08d8517e9db133
Alejandra Buznego <abuznego23@gmail.com>
2022-03-01 12:19:35 -0700

    close range for reported_at_epoch

--- lib/console/packets/packets.ex
+++ lib/console/packets/packets.ex
@@ -10,10 +10,10 @@ defmodule Console.Packets do
     |> Repo.insert()
   end
 
-  def get_packets_per_netid(organization_id, epoch_since) do
+  def get_packets_per_netid(organization_id, epoch_since, current_epoch) do
     query = from p in Packet,
       select: %{reported_at_epoch: p.reported_at_epoch, net_id: p.net_id},
-      where: p.organization_id == ^organization_id and p.reported_at_epoch > ^epoch_since
+      where: p.organization_id == ^organization_id and p.reported_at_epoch > ^epoch_since and p.reported_at_epoch < ^current_epoch
     Repo.all(query)
   end
 end
