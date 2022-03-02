0c2264cedcdfd89bc615b4d0ac97d44fbe381004
Alejandra Buznego <51131939+ambuznego@users.noreply.github.com>
2022-03-02 13:35:16 -0700

    repopulateTotals (#28)

---
+++ priv/repo/migrations/20220302192749_repopulate_org_totals.exs
@@ -0,0 +1,19 @@
+defmodule Console.Repo.Migrations.RepopulateOrgTotals do
+  use Ecto.Migration
+
+  # Repopulating after bug was fixed that made these numbers out of sync
+  def up do
+    results = Ecto.Adapters.SQL.query!(Console.Repo, """
+      SELECT sum(dc_used), count(*), organization_id FROM packets GROUP BY organization_id;
+    """, [], timeout: :infinity).rows
+
+    Enum.each(results, fn row ->
+      Ecto.Adapters.SQL.query!(Console.Repo, """
+        UPDATE organizations SET total_dc = $1, total_packets = $2 WHERE id = $3;
+      """, [Enum.at(row, 0), Enum.at(row, 1), Enum.at(row, 2)])
+    end)
+  end
+
+  def down do
+  end
+end
