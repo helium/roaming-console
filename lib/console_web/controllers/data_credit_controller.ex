c09dc66b0e9ed7f4d1c2292e5a1cde8e91d012b6
Alejandra Buznego <51131939+ambuznego@users.noreply.github.com>
2022-05-18 13:54:49 -0600

    Send individual broadcasts pet net_id to keep / stop purchasing (#56)

--- lib/console_web/controllers/data_credit_controller.ex
+++ lib/console_web/controllers/data_credit_controller.ex
@@ -431,8 +431,9 @@ defmodule ConsoleWeb.DataCreditController do
 
   def broadcast_packet_purchaser_refill_dc_balance(%Organization{} = organization) do
     if organization.dc_balance > 0 do
-      net_id_values = NetIds.get_all_for_organization(organization.id) |> Enum.map(fn n -> n.value end)
-      ConsoleWeb.Endpoint.broadcast("net_id:all", "net_id:all:keep_purchasing", %{ net_ids: net_id_values })
+      NetIds.get_all_for_organization(organization.id) |> Enum.map(fn net_id ->
+        ConsoleWeb.Endpoint.broadcast("net_id:all", "net_id:all:keep_purchasing", %{ net_ids: [net_id.value]})
+      end)
     end
   end
 end
