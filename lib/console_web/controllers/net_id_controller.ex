3bd3724ca42ed07e88116344b9db6a3e33071e04
Alejandra Buznego <abuznego23@gmail.com>
2022-06-10 18:17:13 -0700

    ignore org, will still fail if not allowed to edit net id

--- lib/console_web/controllers/net_id_controller.ex
+++ lib/console_web/controllers/net_id_controller.ex
@@ -10,7 +10,7 @@ defmodule ConsoleWeb.NetIdController do
 
   def update(conn, attrs = %{"id" => id, "protocol" => protocol}) do
     net_id = NetIds.get_net_id!(conn.assigns.current_user, id)
-    organization = Organizations.get_organization!(conn.assigns.current_user, net_id.organization_id)
+    _organization = Organizations.get_organization!(conn.assigns.current_user, net_id.organization_id)
 
     config_attrs = 
       case protocol do
@@ -53,7 +53,7 @@ defmodule ConsoleWeb.NetIdController do
 
   def update(conn, %{"id" => id, "config_id" => config_id, "active" => active}) do
     net_id = NetIds.get_net_id!(conn.assigns.current_user, id)
-    organization = Organizations.get_organization!(conn.assigns.current_user, net_id.organization_id)
+    _organization = Organizations.get_organization!(conn.assigns.current_user, net_id.organization_id)
 
     updated_config = Enum.find(net_id.config, fn c -> c["config_id"] == config_id end) |> Map.put("active", active)
     config = [ updated_config | Enum.filter(net_id.config, fn c -> c["config_id"] != config_id end)]
@@ -79,7 +79,7 @@ defmodule ConsoleWeb.NetIdController do
 
   def remove_config(conn, %{"id" => id, "config_id" => config_id}) do
     net_id = NetIds.get_net_id!(conn.assigns.current_user, id)
-    organization = Organizations.get_organization!(conn.assigns.current_user, net_id.organization_id)
+    _organization = Organizations.get_organization!(conn.assigns.current_user, net_id.organization_id)
 
     config = Enum.filter(net_id.config, fn c -> c["config_id"] != config_id end)
     with {:ok, _} <- NetIds.update_net_id(net_id, %{"config" => config}) do
